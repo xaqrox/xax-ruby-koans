656ba2ce126104cb6e2a3cdaeb3505f0a82917fb
Xaq Rothman <xaq.rox@gmail.com>
2014-05-27 00:45:46 -0400

    168/282

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if [a,b,c].min <= 0
+  x, y, z = [a, b, c].sort
+  raise TriangleError if (x + y) <= z
+
   if a == b and b == c
     return :equilateral
   elsif a == b or b == c or c == a
